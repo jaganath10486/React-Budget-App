6080b77b4af1e239c8bc1ee9e4043e44f9235254
jaganath10486 <jaganathsegi104@gmail.com>
2022-06-01 17:35:26 +0530

    Styling of message

--- src/Task/Message.js
+++ src/Task/Message.js
@@ -1,11 +1,13 @@
-import React, { useContext} from 'react'
+import React, { useContext, useState} from 'react'
 import { AppContext } from '../Context/ContextApi';
 import DisplayMessage from '../Components/Main/DisplayMessage';
+import styles from '../Styles/Message.module.css';
 
 function Message() {
 
+    const [display, setDisplay] = useState(true);
+    console.log(display);
 
-    let display = false;
     const {budget, items} = useContext(AppContext);
 
     let Spent = items.reduce((total, item) => {
@@ -13,19 +15,25 @@ function Message() {
     },0)
 
 
+
     const handleDisplay = () => {
-        display = false;
+        setDisplay(!display);
+        console.log(display);
         console.log('Clicked');
     }
 
   return (
-    <div className='container'>
+    <div className='container' >
+      <div className={display ? styles.show : styles.hide}>
         {
-            ( (budget-Spent) < 20  && (budget-Spent) >= 0 && (<DisplayMessage message = {'The Budget Amount is less than 20 rupees'} classname = {'warning'} handleDisplay = {handleDisplay}/>) )
+            ( (budget-Spent) < 20  && (budget-Spent) >= 0 && (<DisplayMessage message = {'The Budget Amount is less than 20 rupees'} classname = {'warning'} handleDisplay = {handleDisplay} />) )
         }
+      </div>
+      <div className={display ? styles.show : styles.hide}>
         {
-            (budget-Spent < 0) ? (<DisplayMessage message = {'The Budget Amount is less than 0 rupees'} classname = {'danger'} handleDisplay = {handleDisplay}/>) : (<div></div>) 
+           (budget-Spent < 0) ? (<DisplayMessage message = {'The Budget Amount is less than 0 rupees'} classname = {'danger'} handleDisplay = {handleDisplay}/>) : (<div></div>) 
         }
+       </div>
 
     </div>
   )
