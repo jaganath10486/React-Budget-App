baa169630e03dc504e64cef269368da23dd8eb81
jaganath10486 <jaganathsegi104@gmail.com>
2022-06-01 11:07:05 +0530

    Add Budget is done

--- src/Components/Main/Budget.js
+++ src/Components/Main/Budget.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../../Context/ContextApi'
 
 function Budget() {
+    const {dispatch} = useContext(AppContext);
+    const [budget, setBudget] = useState('');
+
+    const handleBudget = (event) => {
+        event.preventDefault();
+        dispatch({
+            type : 'EDIT BUDGET',
+            value : budget}
+        )
+    }
+
   return (
     <div className='container d-flex justify-content-center'>
-        <form>
+        <form onSubmit={handleBudget}>
             <div className='row g-2'>
-            <div class="col-auto">
-               <input type="text" class="form-control" id="inputBudget" placeholder="Enter the Budget Amount" />
-            </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-primary mb-3">Submit</button>
-            </div>
+              <div className="col-auto">
+                <input type="text" className="form-control" id="inputBudget" value={budget}  onChange = {(event) => setBudget(event.target.value)} placeholder="Enter the Budget Amount" />
+              </div>
+              <div className="col-auto">
+                <button type="submit" className="btn btn-primary mb-3">Submit</button>
+              </div>
             </div>
         </form>
     </div>
